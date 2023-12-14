640a373061fae2148de974e343de8b956e1988b3
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-14 14:37:26 +0530

    THIRD

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,7 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {//constructor calling parent
         super(isVeg);
-
+        super.addExtraCheese();
+        super.addExtraToppings();
     }
 }
