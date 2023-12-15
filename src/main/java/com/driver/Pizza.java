805ba0e2d4d199a4e5d9a9bda44522d4dad08861
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-15 14:58:25 +0530

    SeventH

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,5 +1,5 @@
 package com.driver;
-
+/*
 public class Pizza {
 
     private int price;
@@ -62,5 +62,76 @@ public class Pizza {
            billy = true;
        }
         return this.bill;
+    }
+}*/
+public class Pizza {
+    private int price;
+    private Boolean isVeg;
+    private String bill;
+    private final int  cheese=80;
+    private final int vegTopping=70;
+    private final int nonVegTopping=120;
+    private final int bag=20;
+    private boolean cheeseF=false;
+    private boolean toppingF=false;
+    private boolean bagF=false;
+    private boolean billF=false;
+    public Pizza(Boolean isVeg){
+        this.isVeg = isVeg;
+        if(isVeg)
+        {
+            price=300;
+        }
+        else
+        {
+            price=400;
+        }
+
+        this.bill="Base Price Of The Pizza: "+price+"\n";
+        // your code goes here
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public void addExtraCheese(){
+        // your code goes here
+        if(cheeseF==false)
+        {
+            cheeseF=true;
+            price+=80;
+            bill+="Extra Cheese Added: "+cheese+"\n";
+        }
+    }
+
+    public void addExtraToppings() {
+        // your code goes here
+        if(toppingF==false)
+        {
+            toppingF=true;
+            int tempTop= isVeg ? vegTopping : nonVegTopping;
+            price+=tempTop;
+            bill+="Extra Toppings Added: "+tempTop+"\n";
+        }
+    }
+
+    public void addTakeaway(){
+        // your code goes here
+        if(bagF==false)
+        {
+            bagF=true;
+            price+=bag;
+            bill+="Paperbag Added: "+bag+"\n";
+        }
+    }
+
+    public String getBill(){
+        if(billF==false)
+        {
+            bill+="Total Price: "+price+"\n";
+            billF=true;
+        }
+        return bill;
     }
 }
